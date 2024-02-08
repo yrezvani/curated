3e5ad547bcd4c92d498999deb09e994e017206bb
SharpLad <143443228+yrezvani@users.noreply.github.com>
2024-02-08 20:38:09 +0000

    Fix typo in importing Footer.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import "./footer.css";
+import "./Footer.css";
 
 function footer() {
-    return ( <div class=" bg-gray-900">
+    return ( <div className=" bg-gray-900">
     <div className="max-w-2xl mx-auto text-white py-10">
         <div className="text-center">
             <h3 className="text-3xl mb-3"> Download Curated the Art Gallery App </h3>
