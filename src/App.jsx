0e350b01dddccc7385f5a9ad83746c55091f03bb
SharpLad <143443228+yrezvani@users.noreply.github.com>
2024-02-08 20:23:18 +0000

    Add a page for details of artworks Make images in the ClassificationDetail component clickable

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ArtGallery from './components/ArtGallery';
 import ClassificationDetail from './components/ClassificationDetail';
 import Home from './pages/Home'; 
+import ArtworkDetail from './components/ArtworkDetail';
+
 
 const apiKey = '0f4dc3f5-f76a-40aa-adaf-78bcdc8f2e05';
 
@@ -12,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} /> {/* Home page route */}
         <Route path="/art-gallery" element={<ArtGallery apiKey={apiKey} />} />
         <Route path="/classification/:classificationId" element={<ClassificationDetail apiKey={apiKey} />} />
+        <Route path="/artwork/:artworkId" element={<ArtworkDetail apiKey={apiKey} />} />
       </Routes>
     </Router>
   );
