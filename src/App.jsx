996a06d6a6cf1deae2b16dc6f22866ea5e46d89a
SharpLad <143443228+yrezvani@users.noreply.github.com>
2024-02-08 10:54:48 +0000

    Add Home page structure including routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ArtGallery from './components/ArtGallery';
 import ClassificationDetail from './components/ClassificationDetail';
+import Home from './pages/Home'; 
+
 const apiKey = '0f4dc3f5-f76a-40aa-adaf-78bcdc8f2e05';
 
 function App() {
-
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ArtGallery apiKey={apiKey} />} />
+        <Route path="/" element={<Home />} /> {/* Home page route */}
+        <Route path="/art-gallery" element={<ArtGallery apiKey={apiKey} />} />
         <Route path="/classification/:classificationId" element={<ClassificationDetail apiKey={apiKey} />} />
       </Routes>
     </Router>
   );
 }
-
 
 export default App;
