e4844cebb759dd1234a99136cf7d66e94317ba67
lydiabarham <lcbarham9@gmail.com>
2024-02-08 14:19:28 +0000

    Add styling and custom font to jumbotron

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const heading = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,6 +7,9 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      heading: ['Sixtyfour', heading]
+    }
   },
   plugins: [],
 }
