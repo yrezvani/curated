14c45692486f4bbc740d8f3eaba44270babbf69b
Gregory Oghenah <kenny@gregorys-mbp.home>
2024-02-10 21:07:51 +0000

    Completed the getItem from Local storage

--- src/pages/MyGallery.jsx
+++ src/pages/MyGallery.jsx
@@ -2,8 +2,21 @@ import React, { useEffect, useState } from 'react';
 
 const MyGallery = () => {
 
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        const data = JSON.parse(localStorage.getItem('items'));
+        if (data) {
+         setItems(data);
+        }
+    }, []);
+    
+    console.log(items);
+
     return (
-        <div></div>
+        <div>
+            <h1>Hello</h1>
+        </div>
 
     )
 };
