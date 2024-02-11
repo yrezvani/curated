81ccf688006608775bf403278572186f64521ad3
SharpLad <143443228+yrezvani@users.noreply.github.com>
2024-02-11 09:53:45 +0000

    merge main with search branch

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import ClassificationDetail from './components/ClassificationDetail';
 import Home from './pages/Home'; 
 import ArtworkDetail from './components/ArtworkDetail';
 import NavBar from './pages/NavBar';
-import SearchResults from './components/SearchResults';
 
 import Footer from './components/Footer';
 import Contact from './pages/Contact';
@@ -19,18 +18,46 @@ function App() {
       <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/art-gallery" element={<ArtGallery apiKey={apiKey} />} />
-        <Route path="/" element={<Home />} /> {/* Home page route */}
-        <Route path="/explorer" element={<ArtGallery apiKey={apiKey} />} />
-        <Route path="/classification/:classificationId" element={<ClassificationDetail apiKey={apiKey} />} />
-        <Route path="/artwork/:artworkId" element={<ArtworkDetail apiKey={apiKey} />} />
-        <Route path="/search" element={<SearchResults apiKey={apiKey} />} />
         <Route path="/" element={<Home />} /> {/* Home page route */}
         <Route path="/explorer" element={<ArtGallery apiKey={apiKey} />} />
         <Route path="/classification/:classificationId" element={<ClassificationDetail apiKey={apiKey} />} />
         <Route path="/artwork/:artworkId" element={<ArtworkDetail apiKey={apiKey} />} />
         <Route path="/my-gallery" element={<MyGallery />} />  
+        <Route path="contact/*" element={<Contact />} />
+      </Routes>
+      </Router>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ArtGallery from './components/ArtGallery';
+import ClassificationDetail from './components/ClassificationDetail';
+import Home from './pages/Home'; 
+import ArtworkDetail from './components/ArtworkDetail';
+import NavBar from './pages/NavBar';
+import SearchResults from './components/SearchResults';
+
+import Footer from './components/Footer';
+import Contact from './pages/Contact';
+
+const apiKey = '0f4dc3f5-f76a-40aa-adaf-78bcdc8f2e05';
+
+function App() {
+  return (
+    <div className='page-container' >
+      <div className="content-wrap">
+      <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/art-gallery" element={<ArtGallery apiKey={apiKey} />} />
+        <Route path="/classification/:classificationId" element={<ClassificationDetail apiKey={apiKey} />} />
+        <Route path="/artwork/:artworkId" element={<ArtworkDetail apiKey={apiKey} />} />
+        <Route path="/search" element={<SearchResults apiKey={apiKey} />} />
         <Route path="contact/*" element={<Contact />} />
       </Routes>
       </Router>
