256ffe0a5d1339a5c29bebc6b66194af7eb7e8ac
Gregory Oghenah <kenny@gregorys-mbp.home>
2024-02-13 21:36:20 +0000

    Updated the MyGallery Component

--- src/pages/MyGallery.jsx
+++ src/pages/MyGallery.jsx
@@ -1,7 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import './MyGallery.css';
 
 const MyGallery = () => {
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
 
     const [items, setItems] = useState([]);
 
@@ -15,11 +26,12 @@ const MyGallery = () => {
     
 
     return (
-        <div className='my-gallery'>
+        <div className='slider-container'>
+            <Slider {...settings}>
             {items.map(item => (
                 <>
             <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 gallery-item" key={item.id}>
-            <img className="rounded-t-lg" src={item.primaryimageurl} alt={item.title} />
+            <img className="rounded-t-lg artwork-image" src={item.primaryimageurl} alt={item.title} />
             <div className="p-5">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{item.title}</h5>
             {item.period && <p className="mb-3 font-normal text-gray-700 dark:text-gray-400"><strong>Period:</strong> {item.period}</p>}
@@ -30,7 +42,7 @@ const MyGallery = () => {
 
             </>      
             ))}
-            
+            </Slider>
         </div>
 
     );
